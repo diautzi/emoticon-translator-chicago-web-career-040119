cbf18b9b2cd084d8bf8381d552e1eb70bf20603d
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 19:34:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,25 @@
 # require modules here
 require "yaml"
 
-def load_library(path)
-  # code goes here
-  emoticons = YAML.load_file(path)
-  return_hash = {"get_meaning" => {}, "get_emoticon" => {}}
-  emoticons.each do |type, detail|
-    return_hash["get_meaning"][emoticons[type][1]] = type
-    return_hash["get_emoticon"][emoticons[type][0]] = emoticons[type][1]
-  end
-  return_hash
-end
+# def load_library(path)
+#   # code goes here
+#   emoticons = YAML.load_file(path)
+#   return_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+#   emoticons.each do |type, detail|
+#     return_hash["get_meaning"][emoticons[type][1]] = type
+#     return_hash["get_emoticon"][emoticons[type][0]] = emoticons[type][1]
+#   end
+#   return_hash
+# end
 
+def load_library(file_path)
+  library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
+  end
+  library
 
 def get_japanese_emoticon
   # code goes here
