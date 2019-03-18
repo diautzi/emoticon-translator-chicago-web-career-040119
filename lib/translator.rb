58ca068f2c235d250e67e5df3973e1228e066e84
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 19:52:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ def get_japanese_emoticon(path, emoticon)
 
   load_library(path)
   emoticons = YAML.load_file(path)
-  emoticons.each do |type, meaning|
-    if emoticon == emoticons[type[0]]
+  return_hash["get_emoticon"].each do |english, japonese|
+    if emoticon == english
       return emoticons[type[1]]
     else 
       return "Sorry, that emoticon was not found"
