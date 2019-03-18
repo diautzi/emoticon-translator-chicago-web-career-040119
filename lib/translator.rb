47359c791e8077a9784d68f5d073f5056184ae82
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 19:02:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,8 @@ def load_library(path)
   emoticons.each do | key, value|
     new_hash["get_meaning"][value[1]]= key
     new_hash["get_emoticon"][value[0]]= value[1]
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
