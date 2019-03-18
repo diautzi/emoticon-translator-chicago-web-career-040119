e570f316fa56c160cba447beea4958078cc21065
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 19:09:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,9 @@
 # require modules here
 require "yaml"
-#emoticons = YAMl.load_file("lib/emoticons.yml")
 
 def load_library(path)
   # code goes here
+   emoticons = YAMl.load_file(path)
    emoticons.each do | key, value|
     english = value[0]
     japanese= value[1]
